b66eb48bc70a82253c2fba1700bf90e0581d16b7
japinha23 <japinha.criss23@gmail.com>
2025-08-13 11:22:40 -0300

    Add files via upload

---
+++ haha.py
@@ -0,0 +1,8 @@
+import pyautogui # type: ignore
+import time
+pyautogui.press('win')
+pyautogui.write('google chrome')
+pyautogui.press('enter')
+pyautogui.moveTo(1890,30,duration=1)
+
+pyautogui.click()
